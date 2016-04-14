900c66f71288cd71d91ead61dabc7fa9b35aba95
warmans <stefan.warman@gmail.com>
2016-04-14 19:27:58 +0200

    Initial commit

---
+++ resty.go
@@ -0,0 +1,139 @@
+package resty
+
+import (
+	"net/http"
+
+	"golang.org/x/net/context"
+)
+
+type CtxHandler interface {
+	ServeHTTP(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+}
+
+type RESTHandler interface {
+	Before(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandleGet(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandlePost(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandlePut(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandlePatch(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandleDelete(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandleCopy(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandleHead(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandleOptions(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandleLink(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandleUnlink(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandlePurge(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandleLock(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandleUnlock(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandlePropFind(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	HandleView(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+	After(rw http.ResponseWriter, r *http.Request, ctx context.Context)
+}
+
+// -----------------------
+// default handler. embed this in your handler to provide defaults for all the verbs.
+
+type DefaultRESTHandler struct{}
+
+func (h *DefaultRESTHandler) Before(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	//do nothing
+}
+func (h *DefaultRESTHandler) HandleGet(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTHandler) HandlePost(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTHandler) HandlePut(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTHandler) HandlePatch(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTHandler) HandleDelete(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTHandler) HandleCopy(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTHandler) HandleHead(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTHandler) HandleOptions(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTHandler) HandleLink(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTHandler) HandleUnlink(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTHandler) HandlePurge(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTHandler) HandleLock(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTHandler) HandleUnlock(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTHandler) HandlePropFind(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTHandler) HandleView(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	http.Error(rw, "Not Implemented", http.StatusNotImplemented)
+}
+func (h *DefaultRESTHandler) After(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	//do nothing
+}
+
+// ------------------------
+// converter middleware to turn a normal handler into a restful one.
+
+func Restful(next RESTHandler) CtxHandler {
+	return &RestfulConvertMiddleware{NextHandler: next}
+}
+
+// UserRestrictMiddleware redirects user to login page if not logged in
+type RestfulConvertMiddleware struct {
+	NextHandler RESTHandler
+}
+
+func (m *RestfulConvertMiddleware) ServeHTTP(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	m.NextHandler.Before(rw, r, ctx)
+	switch {
+	case r.Method == "GET":
+		m.NextHandler.HandleGet(rw, r, ctx)
+	case r.Method == "POST":
+		m.NextHandler.HandlePost(rw, r, ctx)
+	case r.Method == "PUT":
+		m.NextHandler.HandlePut(rw, r, ctx)
+	case r.Method == "PATCH":
+		m.NextHandler.HandlePatch(rw, r, ctx)
+	case r.Method == "DELETE":
+		m.NextHandler.HandleDelete(rw, r, ctx)
+	case r.Method == "COPY":
+		m.NextHandler.HandleCopy(rw, r, ctx)
+	case r.Method == "HEAD":
+		m.NextHandler.HandleHead(rw, r, ctx)
+	case r.Method == "OPTIONS":
+		m.NextHandler.HandleOptions(rw, r, ctx)
+	case r.Method == "LINK":
+		m.NextHandler.HandleLink(rw, r, ctx)
+	case r.Method == "UNLINK":
+		m.NextHandler.HandleUnlink(rw, r, ctx)
+	case r.Method == "PURGE":
+		m.NextHandler.HandlePurge(rw, r, ctx)
+	case r.Method == "LOCK":
+		m.NextHandler.HandleLock(rw, r, ctx)
+	case r.Method == "UNLOCK":
+		m.NextHandler.HandleUnlock(rw, r, ctx)
+	case r.Method == "PROPFIND":
+		m.NextHandler.HandlePropFind(rw, r, ctx)
+	case r.Method == "VIEW":
+		m.NextHandler.HandleView(rw, r, ctx)
+	default:
+		http.Error(rw, "Unsupported", http.StatusNotImplemented)
+	}
+	m.NextHandler.After(rw, r, ctx)
+}
---
+++ resty_test.go
@@ -0,0 +1,138 @@
+package resty
+
+import (
+	"net/http"
+	"testing"
+
+	"bytes"
+	"fmt"
+	"net/http/httptest"
+
+	"strings"
+
+	"golang.org/x/net/context"
+)
+
+type TestRestHandler struct {
+	DefaultRESTHandler
+}
+
+func (h *TestRestHandler) HandleGet(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "GET")
+}
+func (h *TestRestHandler) HandlePost(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "POST")
+}
+func (h *TestRestHandler) HandlePut(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "PUT")
+}
+func (h *TestRestHandler) HandlePatch(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "PATCH")
+}
+func (h *TestRestHandler) HandleDelete(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "DELETE")
+}
+func (h *TestRestHandler) HandleCopy(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "COPY")
+}
+func (h *TestRestHandler) HandleHead(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "HEAD")
+}
+func (h *TestRestHandler) HandleOptions(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "OPTIONS")
+}
+func (h *TestRestHandler) HandleLink(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "LINK")
+}
+func (h *TestRestHandler) HandleUnlink(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "UNLINK")
+}
+func (h *TestRestHandler) HandlePurge(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "PURGE")
+}
+func (h *TestRestHandler) HandleLock(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "LOCK")
+}
+func (h *TestRestHandler) HandleUnlock(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "UNLOCK")
+}
+func (h *TestRestHandler) HandlePropFind(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "PROPFIND")
+}
+func (h *TestRestHandler) HandleView(rw http.ResponseWriter, r *http.Request, ctx context.Context) {
+	fmt.Fprintf(rw, "VIEW")
+}
+
+func TestRestfulConvertMiddleware(t *testing.T) {
+
+	tests := []struct {
+		Method         string
+		ExpectedBody   string
+		ExpectedStatus int
+	}{
+		{Method: "GET", ExpectedBody: "GET", ExpectedStatus: http.StatusOK},
+		{Method: "POST", ExpectedBody: "POST", ExpectedStatus: http.StatusOK},
+		{Method: "PUT", ExpectedBody: "PUT", ExpectedStatus: http.StatusOK},
+		{Method: "PATCH", ExpectedBody: "PATCH", ExpectedStatus: http.StatusOK},
+		{Method: "DELETE", ExpectedBody: "DELETE", ExpectedStatus: http.StatusOK},
+		{Method: "COPY", ExpectedBody: "COPY", ExpectedStatus: http.StatusOK},
+		{Method: "HEAD", ExpectedBody: "HEAD", ExpectedStatus: http.StatusOK},
+		{Method: "OPTIONS", ExpectedBody: "OPTIONS", ExpectedStatus: http.StatusOK},
+		{Method: "LINK", ExpectedBody: "LINK", ExpectedStatus: http.StatusOK},
+		{Method: "UNLINK", ExpectedBody: "UNLINK", ExpectedStatus: http.StatusOK},
+		{Method: "PURGE", ExpectedBody: "PURGE", ExpectedStatus: http.StatusOK},
+		{Method: "LOCK", ExpectedBody: "LOCK", ExpectedStatus: http.StatusOK},
+		{Method: "UNLOCK", ExpectedBody: "UNLOCK", ExpectedStatus: http.StatusOK},
+		{Method: "PROPFIND", ExpectedBody: "PROPFIND", ExpectedStatus: http.StatusOK},
+		{Method: "VIEW", ExpectedBody: "VIEW", ExpectedStatus: http.StatusOK},
+		{Method: "FOO", ExpectedBody: "Unsupported", ExpectedStatus: http.StatusNotImplemented},
+	}
+
+	handler := &TestRestHandler{}
+
+	for _, test := range tests {
+
+		rw := httptest.NewRecorder()
+		r, err := http.NewRequest(test.Method, "", bytes.NewBuffer([]byte("")))
+
+		if err != nil {
+			t.Errorf("Error building test request: %s", err)
+		}
+
+		//execute the handler
+		Restful(handler).ServeHTTP(rw, r, context.Background())
+
+		if res := strings.TrimSpace(rw.Body.String()); res != test.ExpectedBody {
+			t.Errorf("Unexpected response body! Expected '%s' got '%s'", test.ExpectedBody, res)
+		}
+		if rw.Code != test.ExpectedStatus {
+			t.Errorf("Unexpected status! Expected %d got %d", test.ExpectedStatus, rw.Code)
+		}
+	}
+}
+
+func TestDefaultRESTHandler(t *testing.T) {
+
+	handler := &DefaultRESTHandler{}
+
+	for _, method := range []string{"GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "LINK", "UNLINK", "PURGE", "LOCK", "UNLOCK", "PROPFIND", "VIEW"} {
+
+		rw := httptest.NewRecorder()
+
+		r, err := http.NewRequest(method, "", bytes.NewBuffer([]byte("")))
+		if err != nil {
+			t.Errorf("Error building test request: %s", err)
+		}
+
+		//execute the handler
+		Restful(handler).ServeHTTP(rw, r, context.Background())
+
+		if res := strings.TrimSpace(rw.Body.String()); res != "Not Implemented" {
+			t.Errorf("Unexpected response body!Default should always return 'Not Implemented' actually: '%s'", res)
+		}
+
+		if rw.Code != http.StatusNotImplemented {
+			t.Errorf("Unexpected status! Expected %d got %d", http.StatusNotImplemented, rw.Code)
+		}
+	}
+}
